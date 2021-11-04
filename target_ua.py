4839563d9a57697c550712d37ffd3c8d59138715
Annnnya <91616531+Annnnya@users.noreply.github.com>
2021-11-04 22:14:29 +0200

    commit5

--- target_ua.py
+++ target_ua.py
@@ -7,10 +7,10 @@ def generate_grid():
     """
     generates 5 unique random letters
     """
+    res = []
     letter_list = ['а', 'б', 'в', 'г', 'ґ', 'д', 'е', 'є', 'ж,' 'з',\
     'и', 'і', 'ї', 'й', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т',\
     'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ь', 'ю', 'я']
-    res = []
     while len(res) < 5:
         letter = random.choice(letter_list)
         if letter not in res:
@@ -31,11 +31,11 @@ def get_words(f_name, letters):
                 if len(line[0]) <= 5:
                     if '/n' in line[1] or 'noun' in line[1]:
                         res.append((line[0], "noun"))
-                    if 'adj' in line[1]:
+                    if line[1].startswith('adj') or line[1].startswith('/adj'):
                         res.append((line[0], "adjective"))
                     if '/v' in line[1]:
                         res.append((line[0], "verb"))
-                    if 'adv' in line[1]:
+                    if line[1].startswith('adv') or line[1].startswith('/adv'):
                         res.append((line[0], "adverb"))
     return res
 
@@ -45,7 +45,7 @@ def check_user_words(user_words, language_part, letters, dict_of_words):
     returns right words and missed words
     >>> check_user_words(['ром', 'абродлд', 'сон'], 'noun',\
 ['а','б','в','г','р'], [('ром','noun'), ('сон','noun')])
-    ([], [])
+    (['ром'], ['сон'])
     """
     fine_user_words = []
     right_words = []
@@ -58,4 +58,4 @@ def check_user_words(user_words, language_part, letters, dict_of_words):
             right_words.append(i[0])
         elif i[1]==language_part:
             missed_words.append(i[0])
-    return [], []
+    return right_words, missed_words
